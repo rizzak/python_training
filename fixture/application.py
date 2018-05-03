5bc87bd7842640d59f2479602038ca1bd7693279
Stanislav Pavlov <spavlov@ivi.ru>
2018-05-03 10:15:11 +0300

    Сделать общую для всех тестов фикстуру в файле conftest.py и добавил тест удаления группы

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,8 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(10)
+        self.wd.maximize_window()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
--- fixture/session.py
+++ fixture/session.py
@@ -1,3 +1,6 @@
+import time
+
+
 class SessionHelper:
 
     def __init__(self, app):
