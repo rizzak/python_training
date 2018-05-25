6f5c30cdd6bc69aca29ed5b3bcc3ce38a02b40da
Stanislav Pavlov <spavlov@ivi.ru>
2018-05-25 13:25:24 +0300

    Задание №9: Обеспечить выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -75,3 +75,8 @@ class ContactHelper:
         wd.find_element_by_css_selector('input[value=Delete]').click()
         # accept alert deletion
         wd.switch_to_alert().accept()
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,4 +3,6 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Vasya"))
     app.contact.modify_first_contact(Contact(first_name="New name"))
