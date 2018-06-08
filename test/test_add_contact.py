904f6f8aab0bbeedf90304e738ccf122c1106a96
Stanislav Pavlov <spavlov@ivi.ru>
2018-06-09 01:29:14 +0300

    Параметризовал тесты для создания групп и контактов, но они почему то не стабильно проходят

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import random
+import string
+import pytest
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="",
+                    home_tel="", mobile_tel="", work_tel="", fax="", email="", homepage="", birthday="",
+                    anniversary="", secondary_address="", secondary_tel="", notes="")] + [
+    Contact(first_name=random_string('first_name', 10), middle_name=random_string('middle_name', 10), last_name=random_string('last_name', 10),
+            nickname=random_string('nickname', 10), title=random_string('random_string', 10), company=random_string('company', 10),
+            address=random_string('address', 10), home_tel=random_string('home_tel', 10), mobile_tel=random_string('mobile_tel', 10),
+            work_tel=random_string('work_tel', 10), fax=random_string('fax', 10), email=random_string('email', 10),
+            homepage=random_string('homepage', 10), birthday=random_string('birthday', 10), anniversary=random_string('anniversary', 10),
+            secondary_address=random_string('secondary_address', 10), secondary_tel=random_string('secondary_tel', 10), notes=random_string('notes', 10))
+    for i in range(5)
+]
 
 
-def test_add_contact(app):
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="Vasya", middle_name="Ivanovich", last_name="Petrov", nickname="Bublik",
-                      title="Title", company="IVI", address="Petrovskaya, 33", home_tel="7777777", mobile_tel="8878787",
-                      work_tel="3434332", fax="4635463", email="bublik@mail.ru", homepage="bublik.com", birthday="1986",
-                      anniversary="2000", secondary_address="Mayskaya, 3", secondary_tel="Home", notes="Moy kontakt")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
